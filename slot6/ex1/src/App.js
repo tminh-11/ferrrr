5b4164c0e184213037ff7f8d642ec5e635842240
tminh-11 <minhdeen1408@gmail.com>
2026-01-28 14:12:03 +0700

    up len

---
+++ slot6/ex1/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import TestUseState from './components/TestUseState';
+function App() {
+  return (
+    <div className="App">
+      <TestUseState />
+    </div>
+  );
+}
+
+export default App;
